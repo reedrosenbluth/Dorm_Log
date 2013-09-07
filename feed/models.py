7f2abc548a28c7beb0f912e9cbd91dcb1e8f4a90
Reed Rosenbluth <Reed.Rosenbluth@gmail.com>
2013-09-06 21:39:18 -0400

    return name

--- feed/models.py
+++ feed/models.py
@@ -3,5 +3,7 @@ from django.contrib.auth.models import User
 
 class Group(models.Model):
     name = models.CharField(max_length=200)
-    parent = models.ForeignKey("Group", related_name='children', blank=True)
+    parent = models.ForeignKey("Group", related_name='children', blank=True, null=True)
     administrator = models.ForeignKey(User, related_name='managed_groups')
+    def __unicode__(self):
+        return "%s" % (self.name)
